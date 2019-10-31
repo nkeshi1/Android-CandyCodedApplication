196d9dd6604ee8a5303540f3251ff0819e17b50f
nkeshi1 <nkansahisaac41@gmail.com>
2019-10-31 19:34:39 +0000

    Completed Module

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -41,4 +41,9 @@ public class InfoActivity extends AppCompatActivity {
     // ***
     // TODO - Task 3 - Launch the Phone Activity
     // ***
+    public void createPhoneIntent(View view){
+        Intent phoneIntent = new Intent(Intent.ACTION_DIAL);
+        phoneIntent.setData(Uri.parse("tel:0556510555"));
+        startActivity(phoneIntent);
+    }
 }
